50805db728573d4bc394bd3fd47aa645b98f7135
Giuseppe Bilotta <giuseppe.bilotta@gmail.com>
2020-10-29 19:56:43 +0100

    First actually working implementation

--- init.lua
+++ init.lua
@@ -11,21 +11,30 @@ local timer = 0
 
 
 -- Actual dowsing function for given player wielding given rod
-local function dowse(rod, player, rod_dowsing)
-	local dowsing = rod_dowsing or rod.get_definition().dowsing
+local function dowse(player, rod, rod_dowsing)
+	local dowsing = rod_dowsing or rod:get_definition().dowsing
 	local player_pos = player:get_pos()
 	player_pos.y = player_pos.y + 1
 	player_pos = vector.round(player_pos)
 
-	for spec in dowsing do
+	local found = false
+	for _, spec in pairs(dowsing) do
 		local range = spec.range
 		local nodenames = spec.target
-		local nodes = minetest.find_nodes_in_area(vector.add(player_pos, -range), vector.add(player_pos, range), nodenames)
-		for node, pos in pair(nodes) do
-			local dist = vector.distance(player_pos, pos)
-			minetest.log("action", player:get_player_name() .. " senses " .. node.name .. " at distance " .. tostring(dist))
+		local node_pos = minetest.find_node_near(player_pos, range, nodenames)
+		if node_pos then
+			local node = minetest.get_node(node_pos)
+			local dist = vector.distance(player_pos, node_pos)
+			local msg = player:get_player_name() .. " senses " .. node.name .. " at distance " .. tostring(dist)
+			minetest.chat_send_player(player:get_player_name(), msg)
+			minetest.log("action", msg)
+			found = true
 		end
 	end
+	if not found then
+		minetest.chat_send_player(player:get_player_name(), player:get_player_name() .. " senses nothing")
+	end
+	return nil
 end
 
 minetest.register_globalstep(function(dtime)
@@ -37,24 +46,29 @@ minetest.register_globalstep(function(dtime)
 	timer = timer - interval
 
 	for _, player in pairs(minetest.get_connected_players()) do
-		local rod = player.get_wielded_item()
-		local rod_dowsing = rod.get_definition().dowsing
+		local rod = player:get_wielded_item()
+		local rod_dowsing = rod:get_definition().dowsing
 		if rod_dowsing ~= nil then
-			dowse(rod, player, rod_dowsing)
+			dowse(player, rod, rod_dowsing)
 		end
 	end
 end)
 
+-- Rod items
+
 minetest.register_craftitem("dowsing:rod", {
 	description = S("Dowsing rod"),
+	-- TODO this currently held upside down 8-/
 	inventory_image = "dowsing_rod.png",
 	groups = { dowsing_rod = 1, flammable = 2},
 	stack_max = 1,
 	dowsing = {
 		{ target = "group:water", range = 10 }
 	},
-	on_use = dowse,
+	on_use = function(item, user, pointed_thing) return dowse(user, item) end,
 })
+
+-- Crafting recipes
 
 minetest.register_craft({
 	output = "dowsing:rod",
@@ -64,6 +78,8 @@ minetest.register_craft({
 		{ "group:stick", "", "group:stick" }
 	},
 })
+
+-- Fuel recipes
 
 minetest.register_craft({
 	type = "fuel",
